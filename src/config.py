e89f0f9fc26bdf540e0fa44a5caf037ed51840ea
alfian-bangunindo <bahrul@bangunindo.com>
2025-07-22 11:01:52 +0700

    feat: modularize config constants

---
+++ src/config.py
@@ -0,0 +1,17 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
+KAFKA_TOPIC = os.getenv("KAFKA_TOPIC")
+KAFKA_CONSUMER_GROUP = os.getenv("KAFKA_CONSUMER_GROUP")
+
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+NUM_DEVICES = 5
